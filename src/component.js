f45470fdf3ce75dc0003c8a9cc2d104d4bc71bc6
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-06-21 18:14:12 +0300

    Add hover animation. Add constants for css classes

--- src/component.js
+++ src/component.js
@@ -46,19 +46,21 @@ const Sankey = Vizabi.Component.extend({
   },
 
   readyOnce() {
+    this._initSettings();
     this._initBasics();
     this._initSankey();
   },
 
   _initBasics() {
     this._element = d3.select(this.element);
-    this._svg = this._element.select(".vzb-sankey-svg");
+    this._svg = this._element.select(this.css.dot(this.css.classes.svg));
 
     const formatNumber = d3.format(",.0f");
     this._format = d => `${formatNumber(d)} TWh`;
-    this._color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    this._initSettings();
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    this._color = value => colorScale(value.replace(/ .*/, ""));
+
     this._calculateSize();
   },
 
@@ -80,6 +82,26 @@ const Sankey = Vizabi.Component.extend({
       medium: {},
       large: {},
     };
+
+    this.css = {
+      dot: classNames => (
+        classNames
+          .split(" ")
+          .filter(Boolean)
+          .map(c => `.${c}`)
+          .join(" ")
+      ),
+
+      classes: {
+        svg: "vzb-sankey-svg",
+        nodesContainer: "nodes-container",
+        node: "node",
+        linksContainer: "links-container",
+        link: "link",
+        gradientLinksContainer: "gradient-links-container",
+        gradientLink: "gradient-link"
+      },
+    };
   },
 
   _calculateSize() {
@@ -94,16 +116,24 @@ const Sankey = Vizabi.Component.extend({
 
     this._resizeSankey();
 
+    // TODO: extract styles to css
+    this._nodesContainer = this._svg.append("g")
+      .attr("class", this.css.classes.nodesContainer)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10);
+
     this._linksContainer = this._svg.append("g")
-      .attr("class", "links")
+      .attr("class", this.css.classes.linksContainer)
       .attr("fill", "none")
       .attr("stroke", "#000")
       .attr("stroke-opacity", 0.2);
 
-    this._nodesContainer = this._svg.append("g")
-      .attr("class", "nodes")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10);
+    this._gradientLinksContainer = this._svg.append("g")
+      .attr("class", this.css.classes.gradientLinksContainer)
+      .attr("fill", "none")
+      .attr("stroke", "#000")
+      .attr("stroke-opacity", 0.5);
+
   },
 
 
@@ -139,14 +169,20 @@ const Sankey = Vizabi.Component.extend({
   },
 
   _redrawLinks() {
-    const links = this._linksContainer.selectAll("path")
+    this._redrawDefaultLinks();
+    this._redrawGradientLinks();
+  },
+
+  _redrawDefaultLinks() {
+    const links = this._linksContainer.selectAll(this.css.dot(this.css.classes.link))
       .data(this._graph.links);
 
     links.exit().remove();
 
-    const linksEnter = links.enter().append("path");
+    const linksEnter = links.enter().append("path")
+      .attr("class", this.css.classes.link);
 
-    const mergedLinks = links.merge(linksEnter);
+    const mergedLinks = this._links = links.merge(linksEnter);
 
     const colorScale = this.model.marker.color.getScale();
     mergedLinks
@@ -158,25 +194,96 @@ const Sankey = Vizabi.Component.extend({
       .text(d => d.source.name + " → " + d.target.name + "\n" + this._format(d.value));
   },
 
+  _redrawGradientLinks() {
+    const gradientLinks = this._gradientLinksContainer.selectAll(this.css.dot(this.css.classes.gradientLink))
+      .data(this._graph.links);
+
+    gradientLinks.exit().remove();
+
+    const gradientLinksEnter = gradientLinks.enter().append("path")
+      .attr("class", this.css.classes.gradientLink);
+
+    const mergedGradientLinks = this._gradientLinks = gradientLinks.merge(gradientLinksEnter);
+
+    mergedGradientLinks
+      .attr("d", sankeyLinkHorizontal())
+      .attr("stroke-width", d => Math.max(1, d.width))
+      .each(this._setDash)
+      .style("stroke", d => this._createGradientDefs(d));
+  },
+
+  _createGradientDefs(d) {
+    const [
+      sourceColor,
+      targetColor
+    ] = [
+      d.source.name,
+      d.target.name
+    ].map(name => this._color(name).replace("#", ""));
+
+    const id = `c-${sourceColor}-to-${targetColor}`;
+    if (!this._svg.select(`#${id}`).node()) {
+      const gradient = this._svg.append("defs")
+        .append("linearGradient")
+        .attr("id", id)
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "0%")
+        .attr("spreadMethod", "pad");
+
+      gradient.append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "#" + sourceColor)
+        .attr("stop-opacity", 1);
+
+      gradient.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "#" + targetColor)
+        .attr("stop-opacity", 1);
+    }
+
+    return "url(#" + id + ")";
+  },
+
+  _setDash() {
+    const $this = d3.select(this);
+    const totalLength = $this.node().getTotalLength();
+
+    $this
+      .attr("stroke-dasharray", `${totalLength} ${totalLength}`)
+      .attr("stroke-dashoffset", totalLength);
+  },
+
   _redrawNodes() {
-    const nodes = this._nodesContainer.selectAll("g")
+    const nodes = this._nodesContainer.selectAll(this.css.dot(this.css.classes.node))
       .data(this._graph.nodes);
 
     nodes.exit().remove();
 
-    const nodesEnter = nodes.enter().append("g");
+    const nodesEnter = nodes.enter().append("g")
+      .attr("class", this.css.classes.node);
     nodesEnter.append("rect");
     nodesEnter.append("text");
     nodesEnter.append("title");
 
-    const mergedNodes = nodes.merge(nodesEnter);
+    const mergedNodes = this._nodes = nodes.merge(nodesEnter);
+
+    mergedNodes
+      .on("mouseover", (...args) => this._animateBranch(...args))
+      .on("mouseout", () => {
+        this._gradientLinks.transition();
+        this._gradientLinks
+          .style("opacity", 0)
+          .each(this._setDash);
+      });
 
     mergedNodes.select("rect")
       .attr("x", d => d.x0)
       .attr("y", d => d.y0)
       .attr("height", d => d.y1 - d.y0)
       .attr("width", d => d.x1 - d.x0)
-      .attr("fill", d => this._color(d.name.replace(/ .*/, "")))
+      .attr("fill", d => this._color(d.name))
       .attr("stroke", "#000");
 
     mergedNodes.select("text")
@@ -192,6 +299,28 @@ const Sankey = Vizabi.Component.extend({
 
     mergedNodes.select("title")
       .text(d => d.name + "\n" + this._format(d.value));
+  },
+
+  _animateBranch(nodeData) {
+    const nextLayerNodeData = [];
+    const links = this._gradientLinksContainer.selectAll(this.css.dot(this.css.classes.gradientLink))
+      .filter(gradientData => {
+        const result = nodeData.sourceLinks.includes(gradientData);
+        result && nextLayerNodeData.push(gradientData.target);
+        return result;
+      });
+
+    links
+      .style("opacity", null)
+      .transition()
+      .duration(300)
+      .ease(d3.easeLinear)
+      .attr("stroke-dashoffset", 0)
+      .on("end", () =>
+        nextLayerNodeData.forEach(d =>
+          this._animateBranch(d)
+        )
+      );
   },
 
   _getValues() {
